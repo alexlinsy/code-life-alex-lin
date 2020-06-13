ce6e58515e22b498a754b22cc60b85f17343395c
Shaoyi Lin <shaoyilin@Shaoyis-Mac-mini.local>
2020-06-13 15:52:17 -0300

    Change tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: 'G-2N4ZEJM0F2',
+        trackingId: 'UA-169343118-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
       },
